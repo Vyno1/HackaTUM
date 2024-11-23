338d0b0843ca149fec72b99f921149a6f6161520
maxi <maximilian.armuss@live.de>
2024-11-23 20:14:01 +0100

    option values updated

--- src/main/kotlin/org/jetbrains/plugins/template/Options.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/Options.kt
@@ -1,9 +1,9 @@
 package org.jetbrains.plugins.template
 
 enum class Options(val displayName: String) {
-    SECURITY("Security Testing"),
-    FUNCTIONALITY("Core Functionality"),
-    EDGE_CASES("Edge Cases"),
-    EXCEPTION("Error & Exception Handling"),
-    ADDITIONAL_PROMPTS("Any additional specifications");
+    SECURITY("Security"),
+    FUNCTIONALITY("Correct functionality"),
+    EDGE_CASES("Edge cases"),
+    EXCEPTION("Error & exception handling"),
+    ADDITIONAL_PROMPTS("Additional specifications");
 }
