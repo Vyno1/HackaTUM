9e0bf2b7c4df300b460a69e85b169ad2decff0e1
maxi <maximilian.armuss@live.de>
2024-11-23 21:02:36 +0100

    updatae mock python file

--- src/main/resources/python/generate_tests.py
+++ src/main/resources/python/generate_tests.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+import os
 
-out_file = Path(__file__).parent.parent / "out" / "out"
+out_dir = Path(__file__).parent.parent / "out"
 
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+out_file = out_dir / "out"
 
 test_out = \
 """import os
