b14f37fdfd48c64ddb9d3508b8fb7e39529fec5d
maxi <maximilian.armuss@live.de>
2024-11-23 17:37:32 +0100

    fixed typo

--- src/main/kotlin/org/jetbrains/plugins/template/PythonHandler.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/PythonHandler.kt
@@ -7,7 +7,7 @@ object PythonHandler {
             processBuilder.redirectErrorStream(true)
             val process = processBuilder.start()
             val exitCode = process.waitFor()
-            return if (exitCode == 0) "" else "Something went wrong. (exit code: $exit_code)"
+            return if (exitCode == 0) "" else "Something went wrong. (exit code: $exitCode)"
         } catch (e: Exception) {
             return e.message ?: "Unknown error."
         }
