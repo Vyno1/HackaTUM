1304f60deb0aeb84aa1b4e7a26126a17e2dd44da
MittmannM <maxi.mittmann2@gmail.com>
2024-11-23 17:17:02 +0100

    changed function

--- src/main/resources/scripts/command_line_parser.py
+++ src/main/resources/scripts/command_line_parser.py
@@ -5,7 +5,7 @@ import chat_gpt_communication as comm
 def parse_args():
     parser = argparse.ArgumentParser(description="Chat GPT Parser")
     parser.add_argument("--file_path", required=True, help="Path of input file")
-    parser.add_argument("--function", required=True, help="The function to write the tests for")
+    parser.add_argument("--function_path", required=True, help="The function to write the tests for")
     parser.add_argument("--edge_cases", action="store_true", help="Flag set if edge case tests desired")
     parser.add_argument("--security", action="store_true", help="Flag set if security tests desired")
     parser.add_argument("--exception", action="store_true", help="Flag set if exception tests desired")
@@ -20,7 +20,7 @@ def parse_args():
     params = {}
 
     file_path = args.file_path
-    function = args.function
+    function = args.function_path
     comment = args.comment
     module = args.module
 
@@ -30,15 +30,15 @@ def parse_args():
     params["genral_functionality"] = args.general_functionality
     params["other"] = args.other
 
-    return file_path, function, params, module, comment
+    return file_path, function_path, params, module, comment
 
 
 def main():
-    file_path, function, params, module, comment = parse_args()
+    file_path, function_path, params, module, comment = parse_args()
 
     for key, value in params:
         if value:
-            comm.communicate(key, file_path, function, comment, module)
+            comm.communicate(key, file_path, function_path, comment, module)
 
 
 if __name__ == "__main__":
