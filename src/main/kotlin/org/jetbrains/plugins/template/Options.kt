0ebad5b13df87397d7c5bdc3270f143bc9a6b657
maxi <maximilian.armuss@live.de>
2024-11-23 20:04:01 +0100

    changed test category

--- src/main/kotlin/org/jetbrains/plugins/template/Options.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/Options.kt
@@ -2,8 +2,8 @@ package org.jetbrains.plugins.template
 
 enum class Options(val displayName: String) {
     SECURITY("Security Testing"),
-    FUNCTIONALITY("Functionality & Edge Cases"),
-    TYPE_VALIDATION("Type Validation"),
+    FUNCTIONALITY("Core Functionality"),
+    EDGE_CASES("Edge Cases"),
     EXCEPTION("Error & Exception Handling"),
     ADDITIONAL_PROMPTS("Any additional specifications");
 }
